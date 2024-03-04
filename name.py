6ea51451276dbf884a0e22c2b77547d1335ee8f4
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-03-04 19:44:32 +0530

    Add 2 numbers

--- name.py
+++ name.py
@@ -4,3 +4,10 @@ name = input("Enter your name: ")
 print("your name is",name)
 
 # hello this is just a trail to check 
+
+number1 = input("Enter the first number : ")
+number2 = input("Enter the second number : ")
+
+sum = number1 + number2
+
+print("The sum of both  numbers is ",sum)
